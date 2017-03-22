c7a830ad1bc7ebe6f0b112c68e0a1a327bbce1bf
Jess Scheuring <jscheuring@gmail.com>
2017-03-22 14:52:37 -0500

    move app.js into scripts folder

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,6 @@
 require('file-loader?emitFile=false!./index.html');
 require('./stylesheets/app.scss'); /* This loads the css and foces the sass to compile... */
 require('file-loader?emitFile=false!./images/js-img.png');
-import app from './app.js';
+import app from './scripts/app.js';
 
 app();
