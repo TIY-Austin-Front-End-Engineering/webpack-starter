cf28d28b7ec7c7f53f8edd203ace9b4b85dd62c4
Justin Herrick <justin@lunarcollective.co>
2017-03-29 12:50:33 -0500

    Adding some more helpful comments.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,12 @@ import Store from './store.js'
 
 export default function app() {
   //All of your code starts here
+
+  //Example of the most minimal view possible.
   let defaultView = `<h1>Hello World!</h1>`
 
+  //The default state of this app is nothing, just an empty object.
+  //  Don't worry about adding state until you need to keep track of it across views.
   let initialState = {
   };
 
@@ -16,6 +20,7 @@ export default function app() {
       console.debug(`State: ${state} | Event: '${event}'`);
       return;
     } else {
+      // Your update code goes below here
 
       if (event === "hello_world") {
         console.log("hello world!");
@@ -23,16 +28,20 @@ export default function app() {
         return state;
       }
 
+      // Your update code goes above here
       console.debug(`Unhandled Event: '${event}'`);
       return;
     }
   };
 
   const render = function (state, event, data) {
+    // You will want to update this render function to render
     $('#app').html(defaultView);
   };
 
+  // Every time an event is fired the update function will run
+  //  and *then* the render function will run after that.
   store.add(update);
   store.add(render);
-  store.fire("hello_world");
+  store.fire("hello_world"); // Feel free to delete. This is just an example.
 }
