7e72d24d7978ac2fa0555063de278bee758bc40e
Justin Herrick <justin@lunarcollective.co>
2017-04-06 12:29:32 -0500

    Adding model structure, middleware, and examples.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,25 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddlware } from 'redux'
+import thunk from './middleware/thunk.js'
+import logger from './middleware/logger.js'
+
 export default function app() {
 
-  console.log(createStore(function (state = [], action) {
-    return state;
-  }));
+    const reducer = function (state, action) {
+        switch (action.type) {
+            case "START":
+                return state;
+
+            default:
+                console.debug(`Unhandled Action: ${action.type}!`);
+                return state;
+        };
+    };
+
+    const store = createStore(
+        reducer,
+        applyMiddleware(
+            thunk,
+            logger
+        )
+    );
 }
